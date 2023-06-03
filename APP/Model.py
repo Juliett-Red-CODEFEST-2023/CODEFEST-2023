d382627550a2761528188c160a62756a500dab2c
martin10ca <m.calderona@uniandes.edu.co>
2023-06-03 00:07:40 -0500

    vainas

--- APP/Model.py
+++ APP/Model.py
@@ -1,1 +1,12 @@
+import transformers
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+tokenizer = AutoTokenizer.from_pretrained("MMG/xlm-roberta-large-ner-spanish")
+model = AutoModelForTokenClassification.from_pretrained("MMG/xlm-roberta-large-ner-spanish")
+
+def ReviewData(dic):
+    info=dic["TEXTO"]
+    print(info)
+    text=GetInfo(info)
+    return text
 
